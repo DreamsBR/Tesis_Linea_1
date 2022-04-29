9824e9c53ad6ae551968631af81b3e11ee4ef57b
Marcos Basurto Roca <marcosbr301@gmail.com>
2022-04-29 11:38:14 -0500

    Tipo Producto Controller : ObtenerAllProducts

---
+++ src/main/java/com/lineauno/respository/TipoProductoRepository.java
@@ -0,0 +1,11 @@
+package com.lineauno.respository;
+
+import com.lineauno.entity.TipoProducto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TipoProductoRepository extends JpaRepository<TipoProducto, Integer> {
+
+
+}
